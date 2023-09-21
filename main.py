24056b8a8460e78e2356d69c62545f868d5e4a02
Maxi Rodríguez Herrero <maxi.rodriguez@aligntech.com>
2023-09-21 10:50:42 +0200

    Config Creation Feature Added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 
 from structure_gen.dev_project import DevProject
+from structure_gen.utils import create_config_file
+
 
 parser = argparse.ArgumentParser(description="Automatic Project Structure Generator")
 
@@ -12,11 +14,17 @@ parser.add_argument("--packages", required=False, default=["default"],
                     nargs="*", type=str, help="Packages to be created")
 parser.add_argument("--folders", required=False, default=["test", "images"], 
                     nargs="*", type=str, help="create empty folders")
+parser.add_argument("--config", type=bool, required=False, 
+                    default=False, help="Input Config True if to create the config file")
 
 
 if __name__ == "__main__":
     # Parse and assign the arguments
     args = parser.parse_args()
+
+    if args.config:
+        create_config_file()
+
     project_type = args.type
     output_dir = args.output_dir
     packages = args.packages
