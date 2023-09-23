105d85db1379862b81bee83fd4d3bed3c5fb53b1
MaximoRdz <maximo.rodriguez@estudiante.uam.es>
2023-09-23 19:05:45 +0200

    Try/except config file

--- structure_gen/structure_gen.py
+++ structure_gen/structure_gen.py
@@ -1,14 +1,14 @@
 import os
 
 import structure_gen.constants as ct
-from structure_gen.utils import print_tree, read_json
+from structure_gen.utils import print_tree, read_json, create_config_file
 
 
 class StructureGen:
     """ 
     Parent class capable of parsing main.py args and creating the project
     structure.
-    :param project_type: "AP" for dev projects "DS" for Data Science projects.
+    :param project_type: "DEV" for dev projects "DS" for Data Science projects.
     :param output_dir: Path to locate the workplace, str.
     :param packages: List of packages to create, list(str).
     :param folders: List of emtpy folders to create, list(str).
@@ -19,7 +19,12 @@ class StructureGen:
         self.packages = packages
         self.folders = folders
 
-        self.config_info = read_json(ct.CONFIG_FILE_PATH)
+        try:
+            self.config_info = read_json(ct.CONFIG_FILE_PATH)
+        except FileNotFoundError:
+            create_config_file()
+            self.config_info = read_json(ct.CONFIG_FILE_PATH)
+
         self.header = "\n".join(list(self.config_info["header"].values()))
         self.gitignore = ct.GITIGNORE_LINE_SEP.join(self.config_info["gitignore"])
 
@@ -54,7 +59,7 @@ class StructureGen:
     def create_package(self, package_name):
         """
         Create a folder with python's module structure
-        packgage: __init__.py, utils.py, constants.py, README.md and package.py
+        package: __init__.py, utils.py, constants.py, README.md and package.py
         """
         self.create_folder(package_name)
         package_path = os.path.join(self.output_dir, package_name)
