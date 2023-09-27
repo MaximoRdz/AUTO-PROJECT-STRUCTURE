32cda4f861faa15d92ecebd4e8a3e1666409360b
MaximoRdz <maximo.rodriguez@estudiante.uam.es>
2023-09-27 18:11:57 +0200

    data science project feature

--- structure_gen/constants.py
+++ structure_gen/constants.py
@@ -4,3 +4,4 @@ FILE_JUST = 10    # Justify string for files tree_print()
 FOLDER_JUST = 5    # Justify string for folders tree_print()
 CONFIG_FILE_PATH = os.path.join(".", "config.json")
 GITIGNORE_LINE_SEP = "".join(["\n", "# ", 20*"-", "\n"])
+
